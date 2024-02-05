bcf5e05f3c3544cbba31182a7aae3c5ebf6e1308
Tucker Traywick <tucker.traywick@gmail.com>
2024-02-05 01:41:27 -0500

    Setup list test bed.

--- source/editor.c
+++ source/editor.c
@@ -6,8 +6,8 @@
 // Represents a line in a buffer. Stores a length. Not null-terminated and does not include an '\r'
 // or '\n'.
 struct Line {
-	size_t length; // The current number of characters in the line.
 	size_t capacity; // The number of characters allocated for the line.
+	size_t length; // The current number of characters in the line.
 	char text[1]; // The text of the line. `capacity` characters long.
 };
 typedef struct List LineList;
@@ -46,15 +46,38 @@ struct Editor {
 	size_t currentTabIndex;
 };
 
+// int main(void) {
+// 	initscr();
+// 	raw();
+// 	keypad(stdscr, TRUE);
+// 	noecho();
+
+// 	printw("hi");
+// 	getch();
+
+// 	endwin();
+// 	return 0;
+// }
+
 int main(void) {
-	initscr();
-	raw();
-	keypad(stdscr, TRUE);
-	noecho();
+	struct List list = listCreate(10, sizeof(int));
+	int a = 1;
+	int b = 2;
+	int c = 3;
+	listAppend(&list, &a);
+	listAppend(&list, &b);
+	listAppend(&list, &c);
+	listSwap(&list, 0, 1);
+	int x = 7;
+	listSet(&list, 1, &x);
+	x = 100;
+	listInsert(&list, 2, &x);
 
-	printw("hi");
-	getch();
+	for (size_t i = 0; i < list.length; ++i) {
+		printf("list[%zu] = %d\n", i, *(int*)listGet(&list, i));
+	}
 
-	endwin();
+	listDestroy(&list);
+
 	return 0;
 }
