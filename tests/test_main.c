cdbab3ef07e321b026d0827afcbd3754e5b7cb13
Tucker Traywick <tucker.traywick@gmail.com>
2026-04-20 19:39:09 -0400

    Fixed bugs with assertions in `test_buffer_insert_piece_before()`.

--- tests/test_main.c
+++ tests/test_main.c
@@ -2,20 +2,29 @@
 #include <stdio.h>
 #include "test.h"
 #include "editor.h"
+#include "list.h"
 
-static struct buffer buffer;
+static void test_buffer_initialize_and_destroy(void) {
 
-static void setup(void) {
-	buffer_initialize(&buffer, 1, 1);
-}
+	// struct buffer buffer = {0};
+	// buffer_initialize(&buffer, 1, 1);
+	// assert_eq((void*)buffer.pieces, (void*)buffer.first_piece, "%p", "%p");
+	// assert_eq(list_get_capacity(&buffer.pieces), (size_t)1, "%zu", "%zu");
+	// assert_eq(list_get_count(&buffer.pieces), (size_t)0, "%zu", "%zu");
 
-static void teardown(void) {
-	buffer_destroy(&buffer);
+	// buffer_destroy(&buffer);
+	// assert_eq((void*)buffer.original_text, NULL, "%p", "%p");
+	// assert_eq((void*)buffer.new_text, NULL, "%p", "%p");
+	// assert_eq((void*)buffer.pieces, NULL, "%p", "%p");
+	// assert_eq((void*)buffer.free_pieces, NULL, "%p", "%p");
+	// assert_eq((void*)buffer.first_piece, NULL, "%p", "%p");
 }
 
 static void test_buffer_insert_piece_before(void) {
-	struct piece *previous_first_piece = buffer.first_piece;
-	struct piece *piece1 = buffer_insert_piece_before(&buffer, buffer.first_piece, NULL);
+	struct buffer buffer = {0};
+	buffer_initialize(&buffer, 1, 1);
+
+	struct piece *piece1 = buffer_insert_piece_before(&buffer, 0, NULL);
 	assert_eq(piece1 - buffer.pieces, (size_t)1, "%zu", "%zu");
 	assert_eq(piece1->previous_piece_index, PIECE_NONE, "%u", "%u");
 	assert_eq(piece1->next_piece_index, 0, "%u", "%u");
@@ -32,23 +41,24 @@ static void test_buffer_insert_piece_before(void) {
 			.length = 4,
 		},
 	};
-	struct piece *piece2 = buffer_insert_piece_before(&buffer, previous_first_piece, &piece_value);
+	struct piece *piece2 = buffer_insert_piece_before(&buffer, 0, &piece_value);
 	assert_eq(piece2 - buffer.pieces, (size_t)2, "%zu", "%zu");
-	assert_eq(piece2->previous_piece_index, (uint32_t)(piece1 - buffer.pieces), "%u", "%u");
+	assert_eq(piece2->previous_piece_index, 1, "%u", "%u");
 	assert_eq(piece2->next_piece_index, 0, "%u", "%u");
 	assert_eq(piece2->selection.index, piece_value.selection.index, "%u", "%u");
 	assert_eq(piece2->selection.length, piece_value.selection.length, "%u", "%u");
 	assert_eq(piece2->is_new, 1, "%u", "%u");
 	assert_eq((void*)buffer.first_piece, (void*)piece1, "%p", "%p");
 	assert_eq(buffer.pieces->previous_piece_index, (uint32_t)(piece2 - buffer.pieces), "%u", "%u");
+
+	assert_eq(list_get_count(&buffer.pieces), (size_t)3, "%zu", "%zu");
+	assert_eq(list_get_capacity(&buffer.pieces), (size_t)4, "%zu", "%zu");
+	buffer_destroy(&buffer);
 }
 
-static void test_buffer_get_piece_and_offset(void);
-
 int main(void) {
-	setup();
 	begin_testing();
+		run_test(test_buffer_initialize_and_destroy);
 		run_test(test_buffer_insert_piece_before);
-	teardown();
 	return end_testing();
 }
