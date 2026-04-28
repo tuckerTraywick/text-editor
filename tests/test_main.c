11db07139d48de9ddb169e8f2a359d11d3cd7ba9
Tucker Traywick <tucker.traywick@gmail.com>
2026-04-27 21:57:13 -0400

    Split function in two.

--- tests/test_main.c
+++ tests/test_main.c
@@ -7,14 +7,22 @@
 
 struct buffer buffer;
 
-void test_buffer_create_and_destroy(void) {
+void test_buffer_create(void) {
 	assert(buffer_initialize(&buffer, 1, 1));
+}
 
+void test_buffer_destroy(void) {
 	buffer_destroy(&buffer);
 }
 
+
+
 int main(void) {
 	begin_testing();
-		run_test(test_buffer_create_and_destroy);
+		run_test(test_buffer_create);
+
+
+
+		run_test(test_buffer_destroy);
 	return end_testing();
 }
